629184ae4e478ee27a4d910cea9d7e5d5c13dbc0
Gary Johnson <gary@gjtt.com>
2013-11-24 16:49:31 -0500

    Initial checkin

--- Clippard/AppDelegate.m
+++ Clippard/AppDelegate.m
@@ -15,4 +15,27 @@
     // Insert code here to initialize your application
 }
 
+int i = 0;
+
+- (IBAction)paste:sender {
+    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+    NSArray *classArray = [NSArray arrayWithObject:[NSImage class]];
+    NSDictionary *options = [NSDictionary dictionary];
+    
+    BOOL ok = [pasteboard canReadObjectForClasses:classArray options:options];
+    if (ok) {
+        NSArray *objectsToPaste = [pasteboard readObjectsForClasses:classArray options:options];
+        NSImage *image = [objectsToPaste objectAtIndex:0];
+        
+        NSData *imageData = [image TIFFRepresentation];
+        NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithData:imageData];
+        NSDictionary *imageProps = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:1.0] forKey:NSImageCompressionFactor];
+        imageData = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];
+        NSString *path = [NSString stringWithFormat:@"/Users/garyjohnson/Desktop/test/%@.jpg", [NSNumber numberWithInt:i]];
+        [imageData writeToFile:path atomically:NO];
+        
+        i++;
+    }
+}
+
 @end
